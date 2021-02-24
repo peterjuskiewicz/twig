8410bcdbc326313d60ba9a8ea120dd693954781e
Piotr Juskiewicz <piotrjuskiewicz@Piotrs-MacBook-Pro.local>
2021-02-24 15:27:22 +0000

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# twig
+# Twig programming challenge
+
+## Test
+
+Used Jest testing library. To run tests `jest`
+
+## Running function
+
+To run execute `node index.js`
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const groupArrayElements = require("./utils/group-array");
 
-console.log(groupArrayElements([1, 2, 3, 4, 5, 6, 7, 8, 9], 2));
+console.log(groupArrayElements([1, 2, 3, 4, 5], 2));
